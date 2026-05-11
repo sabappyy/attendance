66540a9dbeb4ce2f82ec43683ab1f97d7f78a1c8
Sabbir Ahmed Bappy <66788881+sabappyy@users.noreply.github.com>
2026-05-11 15:21:06 +0600

    script.js

---
+++ script.js
@@ -0,0 +1,73 @@
+const scriptURL = 'PASTE_SCRIPT_URL';
+
+async function login(){
+
+const employeeId =
+document.getElementById('employeeId').value;
+
+const password =
+document.getElementById('password').value;
+
+const status =
+document.getElementById('status');
+
+const loginBtn =
+document.getElementById('loginBtn');
+
+loginBtn.disabled = true;
+
+const data = {
+
+action:'login',
+employeeId,
+password
+
+};
+
+try{
+
+const response = await fetch(scriptURL,{
+
+method:'POST',
+body:JSON.stringify(data)
+
+});
+
+const result = await response.json();
+
+status.innerHTML = result.message;
+
+if(result.status === 'success'){
+
+localStorage.setItem(
+'employeeId',
+employeeId
+);
+
+localStorage.setItem(
+'employeeName',
+result.name
+);
+
+window.location.href =
+'dashboard.html';
+
+}
+
+else{
+
+loginBtn.disabled = false;
+
+}
+
+}
+
+catch(error){
+
+status.innerHTML = 'Connection Error';
+
+loginBtn.disabled = false;
+
+}
+
+}
